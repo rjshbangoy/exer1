7c52410768d91ef97c931a388c13a80e43675f11
rjshbangoy <142999247+rjshbangoy@users.noreply.github.com>
2023-09-08 10:38:01 +0800

    Update app.js
    changed qs to element

--- script/app.js
+++ script/app.js
@@ -1,19 +1,19 @@
 function displaySum() {
-  let firstNum = Number(document.querySelector('num1').value)
-  let secondNum = Number(document.querySelector('num2').value)
-
+  let firstNum = Number(document.getElementById('num1').value);
+  let secondNum = Number(document.getElementById('num2').value);
+  
   let total = firstNum + secondNum;
   let sum = document.getElementById("sum");
   sum.style.display = "block";
-  sum.innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+  sum.innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total};
 }
 
 function displayProduct() {
-  let firstNum = Number(document.getElementById('num1').value)
-  let secondNum = Number(document.getElementById('num2').value)
+  let firstNum = Number(document.getElementById('num1').value);
+  let secondNum = Number(document.getElementById('num2').value);
 
   let total = firstNum + secondNum;
   let product = document.getElementById("product");
   product.style.display = "block";
-  product.innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+  product.innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}`;
 }
