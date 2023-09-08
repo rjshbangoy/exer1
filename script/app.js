0ce636dcccaf28648360e5b884adcb229d1ba7d9
rjshbangoy <142999247+rjshbangoy@users.noreply.github.com>
2023-09-08 10:39:12 +0800

    Update app.js
    added `

--- script/app.js
+++ script/app.js
@@ -5,14 +5,14 @@ function displaySum() {
   let total = firstNum + secondNum;
   let sum = document.getElementById("sum");
   sum.style.display = "block";
-  sum.innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total};
+  sum.innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}`;
 }
 
 function displayProduct() {
   let firstNum = Number(document.getElementById('num1').value);
   let secondNum = Number(document.getElementById('num2').value);
 
-  let total = firstNum + secondNum;
+  let total = firstNum * secondNum;
   let product = document.getElementById("product");
   product.style.display = "block";
   product.innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}`;
